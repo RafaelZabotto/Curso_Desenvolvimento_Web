17c7f33613f42c5556514b7e5e826e262e108c19
RafaelZabotto <rafaelm.zabotto@gmail.com>
2018-12-11 10:00:31 -0200

    Utilização de CSS e Projeto Final inacabado

---
+++ css/Projeto TecBlog/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="utf-8" />
+    
+        <title>TecBlog - O seu blog de Tecnologia</title>
+        <link rel="stylesheet" type="text/css" href="css/estilo.css"/>
+
+    </head>
+
+        <body>
+
+            <div id="navegacao">
+                <div id="area">
+                    <h1 id="logo"><span class="verde">Tec</span>Blog</h1>
+                    <div id="menu">
+                        <a href="index.php">Home</a>
+                        <a href="games.php">Games</a>
+                        <a href="apps.php">APPs</a>
+                        <a href="eletronicos.php">Eletrônicos</a>
+                    </div>
+                </div>
+            </div>
+
+        </body>
+
+</html>
---
+++ css/links.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+
+        <meta charset="utf-8" />
+    
+        <title>Links</title>
+        <style type="text/css" >
+
+            a{
+                text-decoration: none;
+                /*tira o underline*/
+                padding: 5px;
+            }
+
+            /*Links não visitados - Formatação padrão*/
+            a:link{
+                color: #b9c941;
+            }
+
+            /*Links <visitados*/
+            /*a:visited{
+                color: #c0c0c0;
+            }*/
+
+            /*links Hover, passa o cursor sobre o link*/
+            a:hover{
+                color: #FFF;/*verde escuro*/
+                background: #6d790f;
+            }
+
+            /*links ativos, quando clicado*/
+            a:active{
+                color: #e4f371;
+            }
+
+        </style>
+
+    </head>
+
+        <body>
+
+            <a href="https://google.com.br">Google</a> |
+            <a href="https://www.tecmundo.com.br">Tec Mundo</a> |
+            <a href="https://www.megacurioso.com.br">Mega Curioso</a> |
+            
+        </body>
+
+</html>
