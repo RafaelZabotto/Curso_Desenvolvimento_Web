06442a6c2e442d33ef60dcbc21a5b3eaae15c4c1
RafaelZabotto <rafaelm.zabotto@gmail.com>
2019-01-23 09:05:49 -0200

    Configuração da conexão com o DB

---
+++ twitter_clone/db_connection.php
@@ -0,0 +1,36 @@
+<?php
+
+    class db{
+
+        //host
+        private $host = 'localhost';
+
+
+        //usuario
+        private $usuario = 'root';
+
+        //senha
+        private $senha = '';
+
+        //banco de dados
+        private $database = 'twitter_clone';
+
+        public function conecta_mysql(){
+
+            //criar a conexão
+            //mysqli_connect(localizacao do bd, usuario,senha,banco);
+            $con = mysqli_connect($this->host,$this->usuario,$this->senha,$this->database);
+
+            //ajustar o charset de cominucação entre a aplicação e o banco de dados
+            mysqli_set_charset($con, 'utf8');
+
+             //Verifica se houve erro de conexão
+            if (mysqli_connect_errno()){
+                echo 'Houve um erro ao tentar se conectar ao banco de dados: '.mysqli_connect_error();
+            }
+
+            return $con;
+        }
+
+    }
+?>
--- twitter_clone/registra_usuario.php
+++ twitter_clone/registra_usuario.php
@@ -1,8 +1,25 @@
 <?php
 
-	$_POST['usuario'];
-	$_POST['email'];
-	$_POST['senha'];
+require_once('db_connection.php');
 
+	$usuario = $_POST['usuario'];
+	$email = $_POST['email'];
+	$senha = $_POST['senha'];
 
+	//instanciamos a classe do db e pegamos o retorno na var link
+	$objDB = new db();
+	$link = $objDB->conecta_mysql();
+
+	//sempre aspas simples na clausula dos valores
+	$sql = "INSERT INTO usuarios(usuario,email,senha) VALUES('$usuario','$email','$senha')";
+
+	//executar a query
+	//mysqli_query(conexao,sql);
+
+	if(mysqli_query($link,$sql)){
+		echo "Usuário registrado com sucesso!";
+
+	}else{
+		echo "Erro ao registrar o usuário";
+	}
 ?>
