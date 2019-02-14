dfbc6666d71b4d540610339f12f0b64e3039c7cf
RafaelZabotto <rafaelm.zabotto@gmail.com>
2019-02-14 09:02:13 -0200

    Configuração de Botões

--- twitter_clone/procurar_pessoas.php
+++ twitter_clone/procurar_pessoas.php
@@ -37,10 +37,13 @@
 								data: $('#form_procurar_pessoas').serialize(),
 								success: function(data){
 									$('#pessoas').html(data);
-
+									
 									$('.btn_seguir').click(function(){
 										var id_usuario = $(this).data('id_usuario');
 										
+										$('#btn_seguir_'+id_usuario).hide();
+										$('#btn_deixar_seguir_'+id_usuario).show();
+
 										$.ajax({
 											url: 'seguir.php',
 											method: 'post',
@@ -54,6 +57,9 @@
 									$('.btn_deixar_seguir').click(function(){
 										var id_usuario = $(this).data('id_usuario');
 										
+										$('#btn_seguir_'+id_usuario).show();
+										$('#btn_deixar_seguir_'+id_usuario).hide();
+
 										$.ajax({
 											url: 'deixar_seguir.php',
 											method: 'post',
